d86a7c2a68fe3d4b3ffef8af938fae5551ea4282
patr7247 <pfortunato@esri.com>
2018-05-02 15:57:01 -0700

    Move tech from MVC to Web API

---
+++ WebChatAPI/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebChatAPI
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string name, string message)
+        {
+            Guid id = Guid.NewGuid();
+            Clients.All.broadcastMessage(id, name, message);
+        }
+    }
+}
